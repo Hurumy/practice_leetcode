266bf66172f1086a828587f8425a372f3ed62f45
Hurumy <61591984+Hurumy@users.noreply.github.com>
2024-02-27 16:39:35 +0900

    Create 1228.cpp

---
+++ 1228.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void dynamicProgramming(vector<int>& arr, int head, int dif, vector<int>& memo)
+    {
+        vector<int> indices;
+        int score = 0;
+        int cidx = head;
+        
+        for (int i=0; i<arr.size(); i++)
+        {
+            if (arr[i] == arr[cidx] + diff)
+            {
+                indices.push_back[i];
+                score ++;
+            }
+        }
+        
+        for (int i=0; i<indices.size(); i++)
+        {
+            if (memo[indices[i]] < score)
+                memo[indices[i]] = score;
+        }
+    }
+    int getScore(vector<int>& arr, vector<int>& memo, int index, int diff)
+    {
+        if (memo[index] == 0)
+        {
+            dynamicProgramming(arr, index, diff, memo);
+            return (memo[index]);
+        }
+        else
+            return (memo[index]);
+    }
+    int longestSubsequence(vector<int>& arr, int difference) {
+        int res = 0;
+        int tmp = 0;
+        vector<int> memo(arr.size());
+        
+        for (i=0; i<arr.size(); i++)
+        {
+            tmp = getScore(arr, memo, i, difference);
+            if(res < tmp)
+                res = tmp;
+        }
+        return (res);
+    }
+};
