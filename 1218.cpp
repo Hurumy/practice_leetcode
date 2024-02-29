dd39c0be6fedcd1774f261c0a2850c3fa13062be
Hurumy <61591984+Hurumy@users.noreply.github.com>
2024-02-29 16:42:28 +0900

    TLE

--- 1218.cpp
+++ 1218.cpp
@@ -3,35 +3,28 @@ public:
     void dynamicProgramming(vector<int>& arr, int head, int diff, vector<int>& memo)
     {
         vector<int> indices;
-        int score = 0;
+        int score = 1;
         int cidx = head;
         
         for (int i=head+1; i<arr.size(); i++)
         {
             if (arr[i] == arr[cidx] + diff)
             {
-                indices.push_back(i);
+                memo[i] = score;
                 score ++;
                 cidx = i;
             }
         }
-        
-        for (int i=0; i<indices.size(); i++)
-            memo[indices[i]] = score;
     }
     int longestSubsequence(vector<int>& arr, int difference) {
-        int res = 0;
-        int tmp;
         vector<int> memo(arr.size(), 0);
         
         for (int i=0; i<arr.size(); i++)
         {
             if (memo[i] == 0)
                 dynamicProgramming(arr, i, difference, memo);
-            tmp = memo[i];
-            if (res < tmp)
-                res = tmp;
         }
+        int res = *max_element(memo.begin(), memo.end());
         return (res+1);
     }
 };
