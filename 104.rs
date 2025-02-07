9a7792e69af877da2fdcdd1b3f06291a1bec9017
Hurumy <61591984+Hurumy@users.noreply.github.com>
2025-02-07 18:59:01 +0900

    Create 104.rs

---
+++ 104.rs
@@ -0,0 +1,34 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                cmp::max(
+                    Solution::max_depth(node.borrow().left.clone()),
+                    Solution::max_depth(node.borrow().right.clone()),
+                ) + 1
+            }
+            None => 0,
+        }
+    }
+}
